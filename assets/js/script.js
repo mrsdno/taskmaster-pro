243283dd98206c857e12fd96edde0b89b2c9104e
mrsdno <christine@dno.dev>
2022-04-04 21:37:14 -0400

    updated styling and hid delete bar until dragging

--- assets/js/script.js
+++ assets/js/script.js
@@ -69,6 +69,7 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
@@ -82,7 +83,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -218,16 +219,19 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    
+    $(this).addClass("dropover")
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
-    
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
+    $(this).addClass("dropover-active");
     
   },
   out: function(event) {
-    
+    $(this).removeClass("dropover-active");
     },
   update: function(event) {
     //array to store the task data in
@@ -271,9 +275,11 @@ $("#trash").droppable({
   },
   over: function(event, ui) {
     console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
     console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 })
 
@@ -281,6 +287,11 @@ $("#modalDueDate").datepicker({
   minDate: 1
 });
 
+setInterval(function(){
+  $(".card .list-group-item").each(function(index, el){
+    auditTask(el);
+  });
+}, 1800000);
 
 // load tasks for the first time
 loadTasks();
